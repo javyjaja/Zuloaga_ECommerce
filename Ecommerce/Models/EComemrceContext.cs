57b96251a2311e3fc261072152951380ff0ac0e6
Javier Hernandez Rangel <javierhr_0321@hotmail.com>
2018-11-25 22:45:37 -0600

    Terminado Video 3

---
+++ Ecommerce/Models/EComemrceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class EComemrceContext : DbContext
+    {
+        public EComemrceContext() :base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Ecommerce.Models.Department> Departments { get; set; }
+    }
+}
